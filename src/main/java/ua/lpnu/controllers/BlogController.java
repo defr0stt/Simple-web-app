fbb267cf33c3c562171a72f8b05213fbce8976c6
defr0stt <dima.sb713@gmail.com>
2022-03-12 21:09:39 +0200

    Added detailed blog info and opportunity to add blogs

--- src/main/java/ua/lpnu/controllers/BlogController.java
+++ src/main/java/ua/lpnu/controllers/BlogController.java
@@ -4,9 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ua.lpnu.models.Post;
 import ua.lpnu.repo.PostRepository;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController
@@ -23,5 +28,32 @@ public class BlogController
         model.addAttribute("posts",posts);
         // returned value = name of view
         return "blog-main";
+    }
+
+    @GetMapping("/blog/add")
+    public String blogAdd(Model model){
+        return "blog-add";
+    }
+
+    @PostMapping("/blog/add")
+    public String blogPostAdd(@RequestParam String title, @RequestParam String anons,
+                              @RequestParam String fullText, Model model){
+        Post post = new Post(title, anons, fullText);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String blogDetails(@PathVariable(value = "id") long  id, Model model){
+
+        if(!postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
+
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "blog-info";
     }
 }
--- src/main/java/ua/lpnu/models/Post.java
+++ src/main/java/ua/lpnu/models/Post.java
@@ -16,6 +16,14 @@ public class Post
     private String title, anons, fullText;
     private int views;
 
+    public Post(String title, String anons, String fullText) {
+        this.title = title;
+        this.anons = anons;
+        this.fullText = fullText;
+    }
+
+    public Post() {}
+
     public Long getId() {
         return id;
     }
