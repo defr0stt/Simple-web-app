49202ad21b10cab254ad8f775204b4ae583a8831
defr0stt <dima.sb713@gmail.com>
2022-03-12 11:49:22 +0200

    Initial commit

---
+++ src/test/java/ua/lpnu/WebProjectSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lpnu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProjectSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
